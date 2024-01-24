ced5bab425528cfa63ddc4f6af172cd2d2d4f259
Mansur Tiyes <tiyes06@mail.ru>
2024-01-24 09:49:07 +0000

    Added player class

---
+++ player.js
@@ -0,0 +1,25 @@
+export class Player{
+    #current_score;
+    #player_guess;
+
+    constructor(){
+        this.#current_score = 0;
+        this.#player_guess = "";
+    }
+
+    get currentScore() {
+        return this.#current_score;
+    }
+
+    set currentScore(newScore){
+        this.#current_score = newScore;
+    }
+
+    get playerGuess() {
+        return this.#player_guess;
+    }
+
+    set playerGuess(newGuess) {
+        this.#player_guess = newGuess;
+    }
+}
