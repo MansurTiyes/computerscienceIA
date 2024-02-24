7b27511eeaf37db8ded6ed7c86175a40da8ca14d
Mansur Tiyes <tiyes06@mail.ru>
2024-02-24 16:59:46 +0000

    Game difficulty points problem fixed

--- game.js
+++ game.js
@@ -22,6 +22,7 @@ class Game{
         }
         functionPlot(options);
 
+        // Ties all dom Manipulation functions to their respective buttons (all gamemode buttons, try it and submit button)
         DOMManipulation.addClickEvent('easy',this.gameModeButtonClicked.bind(this,'easy'));
         DOMManipulation.addClickEvent('medium',this.gameModeButtonClicked.bind(this,'medium'));
         DOMManipulation.addClickEvent('hard',this.gameModeButtonClicked.bind(this, 'hard'));
@@ -32,8 +33,11 @@ class Game{
     // function that will be attached to the three gamemode buttons
     gameModeButtonClicked(game_difficulty){
         // Clears all graphs
+        this.game_difficulty = game_difficulty;
         options.data = [];
+        // updates actual function with random generated function from Function class with game_difficulty as an input
         this.function.actualFunction = this.function.generateRandomFunction(game_difficulty);
+        // adds randomly generated function to functionPlot in order to graph the function
         options.data[0] = {
             fn: this.function.actualFunction
         }
@@ -48,10 +52,13 @@ class Game{
         if (Function.isValid(input) == false){
             DOMManipulation.glowRed();
         } else {
+            // the input from input box is saved as player's guess to be calculated later for score and % difference
             this.player.playerGuess = input;
+            // if there are more then 2 already generated graphs, the last one added is removed
             if (options.data.length == 2){
                 options.data[1] = {};
             }
+            // the player's input is graphed
             options.data[1] = {
                 fn: input
             }
@@ -67,14 +74,18 @@ class Game{
             DOMManipulation.glowRed();
         } else {
             this.player.playerGuess = input;
+            // % difference using GameMath class is calculated 
             let percentageResult = GameMath.calculatePercentageDifference(this.player.playerGuess, this.function.actualFunction);
-            //Makes result visible
+            //Makes result visible by changing color to red
             let temporaryDIV = document.getElementById('calculationdisplay');
             temporaryDIV.style.color = 'red';
+            // DOM updates new score to user
             DOMManipulation.updateScore('percent',percentageResult);
             DOMManipulation.updateScore('actualfunctiondisplay',this.function.actualFunction);
+            // player's score is updated inside the software
             this.player.currentScore += GameMath.scoreCalculate(this.game_difficulty, percentageResult);
             DOMManipulation.updateScore('scoreupdate',this.player.currentScore);
+            // displays explanation to user
             DOMManipulation.displayExplanation(this.function.functionType);
         }
     }
